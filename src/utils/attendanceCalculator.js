ee62e6019a5912cd2bde302d0e15f5b1193f019a
mounttowntech-vinoth <mounttowntech@gmail.com>
2026-07-14 12:20:08 +0530

    update command

--- src/utils/attendanceCalculator.js
+++ src/utils/attendanceCalculator.js
@@ -15,7 +15,7 @@ exports.calculateAttendance = (attendance) => {
   .toLocaleDateString("en-US", { weekday: "long" });
 
   if (day === "Saturday") {
-    requiredMinutes = 390; // 7.5 hours after 60 min break
+    requiredMinutes = 390; // 6 hours 30 minutes
   }
 
   const halfDayMinutes = requiredMinutes / 2;
